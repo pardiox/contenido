648316d4fd0307b1fae8c7d0e945cf4ed20333bd
pardiox <39572582+pardiox@users.noreply.github.com>
2020-09-16 03:32:51 -0300

    Revert "Merge branch 'master' of https://github.com/pardiox/contenido"
    This reverts commit 3424322d399395edc32ef213b6bf934ae662c73e, reversing
    changes made to 9b898e57a1c2e0d4aab9a02ee9a462fb69013264.

--- proyecto.sql
+++ proyecto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-09-2020 a las 08:28:26
+-- Tiempo de generación: 14-09-2020 a las 04:43:38
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.5
 
@@ -63,7 +63,7 @@ CREATE TABLE `alumno` (
 
 INSERT INTO `alumno` (`nombre`, `apellido`, `fecha`, `Email`, `password`, `curso`, `verificado`, `ban_profesor`) VALUES
 ('aswd', 'asd', '0000-00-00', '1@gmail.com', '1234', '2do_2da', 0, 0),
-('luc', 'as', '0000-00-00', '1234@gmail.com', '1234', '2do_2da', 1, 1),
+('luc', 'as', '0000-00-00', '1234@gmail.com', '1234', '2do_2da', 1, 0),
 ('asdasd', 'sad', '0000-00-00', '12344@gmail.com', '1', '2do_2da', 0, 0),
 ('matematicas', 'as', '0000-00-00', '126l34@gmail.com', '12', '2do_2da', 0, 0),
 ('luc', 'asd', '0000-00-00', 'a@gmail.com', '12', '2do_2da', 0, 0),
@@ -74,9 +74,9 @@ INSERT INTO `alumno` (`nombre`, `apellido`, `fecha`, `Email`, `password`, `curso
 ('asdasd', 'asd', '0000-00-00', 'as@gmail.com', '1', '2do_2da', 0, 0),
 ('s', 'as', '0000-00-00', 'as2@gmail.com', '', '2do_2da', 0, 0),
 ('aaa', 'as', '0000-00-00', 'lu@gmail.com', '1', '2do_2da', 0, 0),
-('lasa', 'dsad', '2001-11-24', 'lu123@gmail.com', '12345', '2do_2da', 1, 0),
+('lasa', 'dsad', '2001-11-24', 'lu123@gmail.com', '12345', '', 1, 0),
 ('Lucas', 'Guerrero', '1111-11-24', 'lucasg3322@gmail.com', '1234', '2do_2da', 1, 0),
-('asdasd', 'asd', '0000-00-00', 'lucasg3322@gmail.com1', '1', '2do_2da', 1, 0),
+('asdasd', 'asd', '0000-00-00', 'lucasg3322@gmail.com1', '1', '2do_2da', 0, 0),
 ('luc', 'as', '0000-00-00', 'lusg3322@gmail.com', '123', '2do_2da', 0, 0),
 ('12312', '32132', '0000-00-00', 'luucasg@gmail.com', '1234', '3ro_4ta', 1, 0);
 
@@ -90,7 +90,6 @@ CREATE TABLE `alumnos_verificados` (
   `curso` varchar(15) NOT NULL,
   `materia` varchar(15) NOT NULL DEFAULT '0',
   `Email` varchar(30) NOT NULL,
-  `verificacion` int(1) NOT NULL DEFAULT 0,
   `id` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -98,26 +97,15 @@ CREATE TABLE `alumnos_verificados` (
 -- Volcado de datos para la tabla `alumnos_verificados`
 --
 
-INSERT INTO `alumnos_verificados` (`curso`, `materia`, `Email`, `verificacion`, `id`) VALUES
-('2do_2da', 'literatura', '1@gmail.com', 1, 2),
-('2do_2da', 'literatura', 'a@gmail.com', 1, 3),
-('2do_2da', 'aaaa', 'as2@gmail.com', 1, 7),
-('2do_2da', 'literatura', 'as2@gmail.com', 1, 8),
-('2do_2da', 'aaaa', 'lu@gmail.com', 1, 10),
-('2do_2da', 'literatura', 'lu@gmail.com', 1, 11),
-('2do_2da', 'aaaa', 'lucasg3322@gmail.com1', 1, 13),
-('2do_2da', 'literatura', 'as2@gmail.com', 1, 14),
-('2do_2da', 'literatura', 'lu@gmail.com', 1, 15),
-('2do_2da', 'literatura', 'lucasg3322@gmail.com1', 1, 16),
-('2do_2da', 'biologia', '1@gmail.com', 1, 17),
-('2do_2da', 'biologia', 'a@gmail.com', 1, 18),
-('2do_2da', 'biologia', 'as2@gmail.com', 0, 19),
-('2do_2da', 'biologia', 'as2@gmail.com', 0, 20),
-('2do_2da', 'biologia', 'lu@gmail.com', 1, 21),
-('2do_2da', 'biologia', 'lu@gmail.com', 1, 22),
-('2do_2da', 'biologia', 'as2@gmail.com', 0, 24),
-('2do_2da', 'biologia', 'lu@gmail.com', 1, 25),
-('2do_2da', 'biologia', 'lucasg3322@gmail.com1', 1, 26);
+INSERT INTO `alumnos_verificados` (`curso`, `materia`, `Email`, `id`) VALUES
+('2do_2da', 'aaaa', '1234@gmail.com', 1),
+('2do_2da', '0', '1@gmail.com', 2),
+('2do_2da', '0', 'a@gmail.com', 3),
+('2do_2da', 'aaaa', 'as2@gmail.com', 7),
+('2do_2da', '0', 'as2@gmail.com', 8),
+('2do_2da', 'aaaa', 'lu@gmail.com', 10),
+('2do_2da', '0', 'lu@gmail.com', 11),
+('2do_2da', 'aaaa', 'lucasg3322@gmail.com1', 13);
 
 -- --------------------------------------------------------
 
@@ -153,9 +141,7 @@ INSERT INTO `codigo` (`email`, `codigo`) VALUES
 ('ad@gmail.com1', '3614'),
 ('aad@gmail.com1', '0826'),
 ('aa1d@gmail.com1', '6491'),
-('lucasg3322@gmail.com1', '5058'),
-('', '87sp5s'),
-('', '87sp5s');
+('lucasg3322@gmail.com1', '5058');
 
 -- --------------------------------------------------------
 
@@ -185,11 +171,9 @@ INSERT INTO `contenido` (`cod_trabajo`, `titulo`, `material`, `links`, `planes_e
 (2321325, 'probando 12', 'sadsas', 'dsad', 'asdsa', 0x7777772e636f6d, '3ro_4ta ', 'lengua', 'lucasg@gmail.com'),
 (2321326, 'probando 12', 'sadsas', 'dsad', 'asdsa', 0x7777772e636f6d, '3ro_4ta ', 'lengua', 'lucasg@gmail.com'),
 (2321327, 'asdsasa', 'dsa', 'asdsa', 'sad', 0x736161616161, '3ro_4ta ', 'lengua', 'lucasg@gmail.com'),
+(2321328, 'asdsasaaa', 'dsa', 'asdsa', 'sad', 0x736161616161, '', '', 'lucasg@gmail.com'),
 (2321329, 'tp 1', 'ni idea', 'www.facebook.com', 'yaoi', 0x656e2064696369656d627265, '4to_1ra ', 'lengua', 'lucasg@gmail.com'),
-(2321330, 'hola', 'presentacion', 'contenido', 'asdsad.com', 0x31323030312d30332d3239, '2do_2da ', 'matematicas', 'mario@gmail.com'),
-(2321331, 'Tp1', 'formal', 'revisar el', '222.com', 0x313232322d31322d3132, '2do_2da ', 'matematicas', 'mario@gmail.com'),
-(2321332, 'tp1 literatura', 'presentacion', 'leer los ojos del perro siveriano', 'aa.com', '', '2do_2da ', 'literatura', 'mario@gmail.com'),
-(2321333, 'tp4 biologia', 'en folio', 'celulas madres', 'clulas.com', 0x323030312d31322d3132, '2do_2da ', 'biologia', 'mario@gmail.com');
+(2321330, 'hola', 'presentacion', 'contenido', 'asdsad.com', 0x31323030312d30332d3239, '2do_2da ', 'matematicas', 'mario@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -214,12 +198,11 @@ INSERT INTO `materia` (`cod_materia`, `curso`, `Email`, `nombre_materia`, `verif
 (2, '4to_1ra', 'lucasg@gmail.com', 'lengua', '0'),
 (3, '3ro_4ta', 'lucasg@gmail.com', 'lengua', '1'),
 (4, '3ro_6ta', 'lucasg@gmail.com', 'xaxa', '1'),
-(7, '2do_2da', '123@gmail.com', 'filosofia', '1'),
-(8, '2do_2da', '123@gmail.com', 'aa', '1'),
+(5, '2do_2da', 'mario@gmail.com', 'matematicas', '1'),
+(7, '2do_2da', '123@gmail.com', 'filosofia', '0'),
+(8, '2do_2da', '123@gmail.com', 'aa', '0'),
 (9, '2do_2da', '123@gmail.com', 'aaa', '0'),
-(10, '2do_2da', '123@gmail.com', 'aaaa', '1'),
-(11, '2do_2da', 'mario@gmail.com', 'literatura', '1'),
-(12, '2do_2da', 'mario@gmail.com', 'biologia', '1');
+(10, '2do_2da', '123@gmail.com', 'aaaa', '1');
 
 -- --------------------------------------------------------
 
@@ -287,19 +270,19 @@ ALTER TABLE `profesor`
 -- AUTO_INCREMENT de la tabla `alumnos_verificados`
 --
 ALTER TABLE `alumnos_verificados`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `contenido`
 --
 ALTER TABLE `contenido`
-  MODIFY `cod_trabajo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2321334;
+  MODIFY `cod_trabajo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2321331;
 
 --
 -- AUTO_INCREMENT de la tabla `materia`
 --
 ALTER TABLE `materia`
-  MODIFY `cod_materia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `cod_materia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
