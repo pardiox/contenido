66748aa4dde106e789d20dd25aafd5463dcad9fa
m-alvarado <54638885+m-alvarado@users.noreply.github.com>
2020-09-16 01:44:07 -0300

    vista de inicio del profesor

---
+++ vista_profesor_inicio.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    
+    <link rel="stylesheet" href="css/flex/flexboxgrid.min.css">
+    <link rel="stylesheet" type="text/css" href="css/inicio_prof.css">
+    
+</head>
+<body>
+    <header class="header">
+        <h1>E.E.S.T N°14</h1>
+    </header>
+    
+    <div class="contenedor_principal row center-xs">
+    <div class="bienvenido">
+    <h1 class="col-xs-12">Bienvenido</h1>
+    </div>
+     
+      <div class="cuadros col-xs-12">
+       
+        <div class="row center-xs">   
+            <div class="opciones alumnos col-xs-11 col-lg-4">
+               <a href="" blank="target" title="Alumnos">
+                <h2>Alumnos</h2>
+                <p class="texto_alumnos">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, enim, deleniti! Consectetur natus nemo, ab nam deserunt adipisci ipsum quod velit error, id, fugiat beatae, optio omnis similique earum fugit.</p>
+                </a>
+            </div>
+        
+            <div class="opciones clases col-xs-11 col-lg-4">
+                <h2>Clases</h2>
+                <p class="texto_clases">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus temporibus fuga beatae voluptates qui nihil id ad dolor maiores, sint velit nemo dicta rerum alias cumque ipsa perspiciatis in architecto!</p>
+            </div>  
+        </div>
+        <div class="row center-xs">
+            <div class="opciones cargar_clase col-xs-11 col-lg-4">
+                <h2>Cargar Clases</h2>
+                <p class="texto_cargar">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum est delectus, ipsam optio, repellendus sed laboriosam dignissimos molestias autem suscipit numquam quaerat asperiores rerum cumque omnis maiores minima. Alias, provident.</p>
+            </div>
+        
+            <div class="opciones indefinido col-xs-11 col-lg-4 ">
+                <h2>????</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, sequi. Deserunt libero ratione saepe autem sapiente accusantium dolorem id ex, harum nihil qui, nesciunt culpa doloribus rem reprehenderit voluptate non.</p>
+            </div>
+        </div>
+      </div>
+    </div>
+    
+    <footer></footer>
+</body>
+</html>
