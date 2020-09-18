584305d724df5a478bbb5a5956c4f08531bcb357
LucasG32 <lucasg3322@gmail.com>
2020-09-18 09:52:18 -0300

    Create recuperar_contraseña.php

---
+++ clave/recuperar_contraseña.php
@@ -0,0 +1,19 @@
+<html>
+    <head></head>
+    <header></header>
+    <body>
+        <p>ingresa tu correo electronico o nombre de usuario</p>
+        <form action="enviar_codigo.php" metod="post">
+            <input type="text" value=""placeholder="Email" name="Email">
+            <input type="submit" value="enviar">
+        </form>
+        <?php
+        echo phpversion();
+        ?>
+        
+        
+        
+        
+        
+    </body>
+</html>
