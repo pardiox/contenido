759f0e19dfa3cf7543cc53a6d5889fe1071d6700
LucasG32 <lucasg3322@gmail.com>
2020-09-18 11:27:20 -0300

    Update verificar_codigo.php

--- clave/verificar_codigo.php
+++ clave/verificar_codigo.php
@@ -2,17 +2,25 @@
     <head></head>
     <header></header>
     <body>
-    <form action="verificar_codigo.php" metod="post">
-        <input type="text">
-    </form>    
+  
+        <form action="verificar_codigo.php" metod="post">
+            <input type="text" placeholder="codigo" name="codigo">
+            <input type="submit" value="enviar">
+        </form>   
         
-        
-        
-        
-        
-        
-        
-        
-        
+<?php
+        if(isset($_REQUEST['codigo'])){
+             session_start();
+             $codigo=$_REQUEST['codigo'];
+             include "../conexion.php";
+             $Email=$_SESSION["Email"]; //v
+             $sql =  "SELECT * FROM recuperar_clave where Email='$Email' and codigo='$codigo'";
+             if ($resultad = $con->query($sql)){
+             while ($fila = mysqli_fetch_array($resultad)){
+             $_SESSION["id"]=$fila["id"];
+             
+             header ("location: cambiar_contraseña.php");
+        }}}
+?>
     </body>
 </html>
