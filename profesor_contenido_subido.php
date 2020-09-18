cc12e2c2af8722824335616b05a8b08bb79041fc
LucasG32 <lucasg3322@gmail.com>
2020-09-18 19:35:49 -0300

    Create profesor_contenido_subido.php

---
+++ profesor_contenido_subido.php
@@ -0,0 +1,26 @@
+<?php 
+    session_start();
+	include "conexion.php";
+
+    $fecha_de_entrega=$_REQUEST['fecha_de_entrega'];
+	$titulo=$_REQUEST['titulo'];
+	$curso=$_REQUEST['curso'];
+	$materia=$_REQUEST['materia'];
+	$contenido=$_REQUEST['contenido'];
+    $bibiografia=$_REQUEST['bibiografia'];
+    $Email=$_SESSION["u"]['Email'];
+
+echo "$titulo $fecha_de_entrega $curso $materia $contenido $materia $bibiografia";
+ 
+	$SQL="INSERT INTO contenido (fecha_de_entrega, titulo, curso, materia, contenido, bibiografia, Email) VALUES
+	('$fecha_de_entrega', '$titulo', '$curso', '$materia', '$contenido','$bibiografia','$Email')";
+	
+	if(!$error= $con-> query($SQL)){
+		 echo $con->error;
+	}
+	else{
+	header ("location:clases.php");
+	}
+?>
+	
+	
