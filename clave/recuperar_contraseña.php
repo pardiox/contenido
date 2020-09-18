73f49e07b2dcb046f913d83c84be434c07fd5441
LucasG32 <lucasg3322@gmail.com>
2020-09-18 13:31:10 -0300

    Update recuperar_contraseña.php

--- clave/recuperar_contraseña.php
+++ clave/recuperar_contraseña.php
@@ -1,12 +1,21 @@
 <html>
-    <head></head>
-    <header></header>
+    <head>	
+        <meta charset="UTF-8">
+	    <link rel="stylesheet" href="css/estilos.css">
+    </head>
     <body>
+
         <p>ingresa tu correo electronico o nombre de usuario</p>
         <form action="enviar_codigo.php" metod="post">
             <input type="text" value=""placeholder="Email" name="Email">
             <input type="submit" value="enviar">
         </form>    
-        
+        <?php
+        session_start();
+        if ((isset($_SESSION["noexiste"])) && ($_SESSION["noexiste"]==1)){
+            $_SESSION["noexiste"]=0;
+            echo "el email no existe";
+        }
+        ?>
     </body>
 </html>
