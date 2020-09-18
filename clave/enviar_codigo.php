1cb0ea90b67f6c0cbfa9b39fb7bdb8a7030a6c04
LucasG32 <lucasg3322@gmail.com>
2020-09-18 13:31:05 -0300

    Update enviar_codigo.php

--- clave/enviar_codigo.php
+++ clave/enviar_codigo.php
@@ -1,4 +1,4 @@
-<?php
+    <?php
 include "../conexion.php";
 session_start();
 $Email=$_REQUEST["Email"];
@@ -8,18 +8,53 @@ function generarCodigo($longitud) {
  $pattern = '1234567890';
  $max = strlen($pattern)-1;
  for($i=0;$i < $longitud;$i++) $key .= $pattern{mt_rand(0,$max)};
- return $key;}
+ return $key;
+}
 $codigo = generarCodigo(4);
 
-$sql = "INSERT INTO recuperar_clave(Email, codigo) VALUES ('$Email', '$codigo')";
-if(!$error= $con-> query($sql)){
-	 echo $con->error;
-}
-else{
-    $titulo="codigo de confirmacion";
-    $body="su codigo de confirmacion es $codigo";
-    $location="location: verificar_codigo.php";
-    include("prueba.php");
-}
+            $sql =  "SELECT * from profesor where Email='$Email'";
+            if ($resultado = $con->query($sql)){
+                if ($resultado->num_rows > 0){
+                    $_SESSION["tipo"]="profesor"; 
+                    $tipo =  $_SESSION["tipo"]; 
+                }
+                else{
+                    $SQL =  "SELECT * from alumno where Email='$Email'";
+                    if ($resultad = $con->query($SQL)){
+                        if ($resultad->num_rows > 0){
+                            $_SESSION["tipo"]="alumno"; 
+                            $tipo = $_SESSION["tipo"];
+                        }
+                    }
+                }
+            }
 
+            if (isset($tipo)){
+                if (($tipo=="profesor") or ($tipo == "alumno")){
+                    $SQL =  "SELECT * from recuperar_clave where Email='$Email'";
+                        if ($resultad = $con->query($SQL)){
+                            if ($resultad->num_rows > 0){
+                                $SQL="UPDATE recuperar_clave SET codigo='$codigo' where Email='$Email'";
+                                    if(!$error= $con-> query($SQL)){
+                                        echo $con->error;
+                                    }
+                            }else{
+                                $sql = "INSERT INTO recuperar_clave(Email, codigo) VALUES ('$Email', '$codigo')";
+                                if(!$error= $con-> query($sql)){
+                                    echo $con->error;
+                                }
+                            }
+                        }
+                        $titulo="codigo de confirmacion";
+                        $body="su codigo de confirmacion es $codigo";
+                        $location="location: verificar_codigo.php";
+                        include("prueba.php");  
+                        exit();
+                    }
+                }
+                else{
+                    $_SESSION["noexiste"]=1;
+                    header ("location: recuperar_contraseña.php");  
+                    exit();
+                }
 ?>
