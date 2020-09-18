b726060058bf46d46f557082871db2794c39a5f4
LucasG32 <lucasg3322@gmail.com>
2020-09-18 09:52:31 -0300

    Create prueba.php

---
+++ clave/prueba.php
@@ -0,0 +1,46 @@
+<?php
+ include ("Mailer/src/PHPMailer.php");
+ include ("Mailer/src/SMTP.php");
+ include ("Mailer/src/Exception.php");
+    
+    
+    
+    try {
+    $mail = new PHPMailer\PHPMailer\PHPMailer();
+    echo "hola";
+    $fromeemail="emailprueba869@gmail.com";
+    $fromname="prueba";
+    $host="smtp.gmail.com";  
+    $port="587";
+    $SMTPAuth="login";
+    $SMTPSecure="tls"; 
+    $password="cuentaprueba" ;   
+        
+    $bodyemail="lucasg3322@gmail.com";
+    $subject="que tal";
+    $bodyEmail="mensaje";
+        
+    $mail->isSMTP();             
+    $mail->SMTPDebug  = 0;
+    $mail->Host       = $host;
+    $mail->Port       = $port;
+    $mail->SMTPAuth   = $SMTPAuth;
+    $mail->SMTPSecure = $SMTPSecure;
+    $mail->Username   = $fromeemail;                     
+    $mail->Password   = $password;                               
+
+    //Recipients
+    $mail->setFrom($fromeemail, $fromname);
+    $mail->addAddress($Email);     // Add a recipient
+    
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = $titulo;
+    $mail->Body    = $body;
+
+
+    } 
+    catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+?>
