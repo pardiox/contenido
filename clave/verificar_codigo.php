1f1b77f414890e67b7078dcea35c20e7f224a457
LucasG32 <lucasg3322@gmail.com>
2020-09-18 09:52:22 -0300

    Create verificar_codigo.php

---
+++ clave/verificar_codigo.php
@@ -0,0 +1,18 @@
+<html>
+    <head></head>
+    <header></header>
+    <body>
+    <form action="verificar_codigo.php" metod="post">
+        <input type="text">
+    </form>    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    </body>
+</html>
