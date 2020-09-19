4c4a480815cade6ef49610fbde5e3eedb4de2206
LucasG32 <lucasg3322@gmail.com>
2020-09-19 00:17:22 -0300

    Update main.js

--- admin/js/main.js
+++ admin/js/main.js
@@ -16,7 +16,7 @@ function buscar_datos(consulta){
 }
 
 
-$(document).on('keyup','#caja_busqueda', function(){
+$(document).on('click keyup','#caja_busqueda', function(){
 	var valor = $(this).val();
 	if (valor != "") {
 		buscar_datos(valor);
@@ -46,11 +46,38 @@ function buscar_datos1(consulta1){
 }
 
 
-$(document).on('keyup','#caja_busqueda1', function(){
+$(document).on('click keyup','#caja_busqueda1', function(){
 	var valor1 = $(this).val();
 	if (valor1 != "") {
 		buscar_datos1(valor1);
 	}else{
 		buscar_datos1();
+	}
+});
+
+$(buscar_datos2());
+
+function buscar_datos2(consulta2){
+	$.ajax({
+		url: 'buscar_curso.php' ,
+		type: 'POST' ,
+		dataType: 'html',
+		data: {consulta2: consulta2},
+	})
+	.done(function(respuesta){
+		$("#datos2").html(respuesta);
+	})
+	.fail(function(){
+		console.log("error");
+	});
+}
+
+
+$(document).on('click keyup','#caja_busqueda2', function(){
+	var valor2 = $(this).val();
+	if (valor2 != "") {
+		buscar_datos2(valor2);
+	}else{
+		buscar_datos2();
 	}
 });
