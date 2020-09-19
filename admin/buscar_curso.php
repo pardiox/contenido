b56dfedcc4321544bde2d6242c107b179e787fa5
LucasG32 <lucasg3322@gmail.com>
2020-09-18 22:20:09 -0300

    Create buscar_curso.php

---
+++ admin/buscar_curso.php
@@ -0,0 +1,53 @@
+<?php
+    include "../conexion.php";
+
+
+    $query = "SELECT * FROM cursos_y_materia";    
+    if (isset($_POST['consulta'])) {
+    	$q = $_POST['consulta'];
+    	$query = "SELECT * FROM  cursos_y_materia WHERE curso='$q'";
+    
+
+    $resultado = $con->query($query);
+
+    if ($resultado->num_rows>0) {
+        
+        echo "<select  name='materia' id='curso' required>";
+    	while ($fila = $resultado->fetch_assoc()) {
+    		?>                            
+                   
+                    <option><?php echo $fila['materia']; ?></option>
+
+               
+            <?php
+       
+    	}
+         echo "</select>";
+    }
+        $con->close();
+    }
+    if (isset($_POST['consulta1'])) {
+    	$qq = $_POST['consulta1'];
+    	$query = "SELECT * FROM  cursos_y_materia WHERE curso='$qq'";
+    
+
+    $resultado = $con->query($query);
+
+    if ($resultado->num_rows>0) {
+        
+        echo "<select  name='materia1' id='curso' required>";
+    	while ($fila = $resultado->fetch_assoc()) {
+    		?>                            
+                   
+                    <option><?php echo $fila['materia']; ?></option>
+
+               
+            <?php
+       
+    	}
+         echo "</select>";
+    }
+
+        $con->close();
+    }
+?>
