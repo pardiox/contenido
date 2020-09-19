c952b93dc2419ebdc077e79d259abbfc3b6ffd5f
LucasG32 <lucasg3322@gmail.com>
2020-09-19 00:17:02 -0300

    Update buscar_curso.php

--- admin/buscar_curso.php
+++ admin/buscar_curso.php
@@ -50,4 +50,28 @@
 
         $con->close();
     }
+    if (isset($_POST['consulta2'])) {
+    	$qqq = $_POST['consulta2'];
+    	$query = "SELECT * FROM  cursos_y_materia WHERE curso='$qqq'";
+    
+
+    $resultado = $con->query($query);
+
+    if ($resultado->num_rows>0) {
+        
+        echo "<select  name='materia1' id='curso' required>";
+    	while ($fila = $resultado->fetch_assoc()) {
+    		?>                            
+                   
+                    <option><?php echo $fila['materia']; ?></option>
+
+               
+            <?php
+       
+    	}
+         echo "</select>";
+    }
+
+        $con->close();
+    }
 ?>
