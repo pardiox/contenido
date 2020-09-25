9743ce93dc02654a59f32743ced367db2fd913d3
LucasG32 <lucasg3322@gmail.com>
2020-09-25 06:09:39 -0300

    Create main1.js

---
+++ profesor/js/main1.js
@@ -0,0 +1,26 @@
+
+
+function buscar_datos(consulta){
+	$.ajax({
+		url: 'buscar_curso.php' ,
+		type: 'POST' ,
+		dataType: 'html',
+		data: {consulta: consulta},
+	})
+	.done(function(respuesta){
+		$("#datos").html(respuesta);
+	})
+	.fail(function(){
+		console.log("error");
+	});
+}
+
+
+$(document).on('keyup click','#caja_busqueda', function(){
+	var valor = $(this).val();
+	if (valor != "") {
+		buscar_datos(valor);
+	}else{
+		buscar_datos();
+	}
+});
