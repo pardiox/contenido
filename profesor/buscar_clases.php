533ab26f1d0e8da1d8eb537e5e7c0c82c93df78b
LucasG32 <lucasg3322@gmail.com>
2020-09-25 06:09:14 -0300

    Create buscar_clases.php

---
+++ profesor/buscar_clases.php
@@ -0,0 +1,29 @@
+<?php
+    include "../conexion.php";
+
+
+    $query = "SELECT * FROM cursos_y_materia";    
+    if (isset($_POST['consulta'])) {
+    	$q = $_POST['consulta'];
+    	$query = "SELECT * FROM  cursos_y_materia WHERE curso='$q'";
+    
+
+    $resultado = $con->query($query);
+
+    if ($resultado->num_rows>0) {
+        
+        echo "<select  class='select'name='materia' id='curso' required>";
+    	while ($fila = $resultado->fetch_assoc()) {
+    		?>                            
+                   
+                    <option><?php echo $fila['materia']; ?></option>
+
+               
+            <?php
+       
+    	}
+         echo "</select>";
+    }
+        $con->close();
+    }
+  ?>
