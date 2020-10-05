8fa44b0c03977c18016f7b4c2d8c1f52421217d0
m-alvarado <54638885+m-alvarado@users.noreply.github.com>
2020-10-05 03:12:54 -0300

    se ordenó el código
    hace falta arreglar el php

--- profesor/lista_de_alumnos.php
+++ profesor/lista_de_alumnos.php
@@ -19,113 +19,141 @@
     <header>  
       <h1>E.E.S.T.N°14</h1>
     </header> 
-      <div class="contendor_principal row around-md ">
-      <div class="peticiones col-md-5 col-xs-12">
-      <?php
-    session_start();
-  if( $_SESSION["u"]['sesion'] != "s.p"){
-    header ("location: login.php");
-    }
-   
-include "../conexion.php";
-    $Email=$_SESSION["u"]['Email'];
-    $sq =  "SELECT * FROM cursos_y_materia where profesor='$Email'";
-    if ($resultad = $con->query($sq)){
-    while ($fila = mysqli_fetch_array($resultad)){
+    <div class="contendor_principal row around-md ">
+        <div class="peticiones col-md-5 col-xs-12">
         
-    $materia=$fila["materia"];
-    $curso=$fila["curso"];
-    $sql =  "SELECT * FROM alumnos_verificados where curso='$curso' and materia='$materia'"; /*and verificacion='0'";*/
-if ($resultado = $con->query($sql)){
-    while ($fila = mysqli_fetch_array($resultado)){
-        $curso=$fila["curso"];
-        $materia=$fila["materia"];
-        $Email=$fila["Email"];
-        $_SESSION["u"]['materia']=$materia;
-       
-        
-        $ssql = "SELECT * FROM alumno where Email='$Email'";
-        if($resultadross = $con->query($ssql)){
-            if($resultadross ->num_rows > 0){
-               $fila=$resultadross->fetch_assoc(); 
-                $nombre=$fila["nombre"];
-                $apellido=$fila["apellido"];
-            }
+        <?php      
+        session_start();
+        if( $_SESSION["u"]['sesion'] != "s.p"){
+            header ("location: login.php");
         }
-        echo"
+   
+        include "../conexion.php";
+            
+        $Email=$_SESSION["u"]['Email'];
+        $sq =  "SELECT * FROM cursos_y_materia where profesor='$Email'";
+            
+        if ($resultad = $con->query($sq)){
+            while ($fila = mysqli_fetch_array($resultad)){
         
-        <div class='contenedor-tabla row around-xs'> 
-        <table class='col-xs-12'>
-           <tr >
-              <td class='col-xs-3'>  
-            ". $nombre ."<br> ". $apellido."
-              </td>  
-              <td class='col-xs-2'>
-            ".  $curso."
-              </td>
-              <td class='col-xs-2'>  
-            ".  $materia."
-              </td> 
-              <td class='col-xs-2'>
-              <a href='verificado_denegado.php?Email=$Email & materia=$materia' > <button>denegar</button></a> </td>
-              <td  class='col-xs-2'>
-              <a href='verificado_alumno.php?Email=$Email & materia=$materia'> <button>verificar</button></a> </td>
-          </tr>
-        </table>
-        </div>";
+                $materia=$fila["materia"];
+                $curso=$fila["curso"];
+                $sql =  "SELECT * FROM alumnos_verificados where curso='$curso' and materia='$materia'"; /*and verificacion='0'";*/
+                
+                if ($resultado = $con->query($sql)){
+                    
+                    while ($fila = mysqli_fetch_array($resultado)){
+                        
+                        $curso=$fila["curso"];
+                        $materia=$fila["materia"];
+                        $Email=$fila["Email"];
+                        $_SESSION["u"]['materia']=$materia;
+                        $ssql = "SELECT * FROM alumno where Email='$Email'";
+             
+                    if($resultadross = $con->query($ssql)){
+                        if($resultadross ->num_rows > 0){
+                            
+                            $fila=$resultadross->fetch_assoc(); 
+                            $nombre=$fila["nombre"];
+                            $apellido=$fila["apellido"];
+                        }
+                    }  
+
+                    echo"
+        
+                        <div class='contenedor-tabla row around-xs'> 
+                            <table class='col-xs-12'>
+                                <tr >
+                                    <td class='col-xs-3'>  
+                                        ". $nombre ."<br> ". $apellido."
+                                    </td>  
+                                    
+                                    <td class='col-xs-2'>
+                                        ".  $curso."
+                                    </td>
+                                    
+                                    <td class='col-xs-2'>  
+                                        ".  $materia."
+                                    </td> 
+                                    
+                                    <td class='col-xs-2'>
+                                        <a href='verificado_denegado.php?Email=$Email & materia=$materia' > <button>denegar</button></a> </td>
+                                    <td  class='col-xs-2'>
+                                    <a href='verificado_alumno.php?Email=$Email & materia=$materia'> <button>verificar</button>
+                                    </a>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>";
 }}
-}}   
+                }}   
 ?>
 </div> 
-<div class="alu_aceptados col-xs-12 col-md-5">
-          <?php
-    $Email=$_SESSION["u"]['Email'];
-    $sqll =  "SELECT * FROM cursos_y_materia where profesor='$Email'";
-    if ($resultadios = $con->query($sqll)){
-    while ($fila = mysqli_fetch_array($resultadios)){
+        <div class="alu_aceptados col-xs-12 col-md-5">
+        <?php
+            
+        $Email=$_SESSION["u"]['Email'];
+        $sqll =  "SELECT * FROM cursos_y_materia where profesor='$Email'";
+            
+        if ($resultadios = $con->query($sqll)){
+            
+            while ($fila = mysqli_fetch_array($resultadios)){
         
-    $materia=$fila["materia"];
-    $curso=$fila["curso"];  
-    $sl =  "SELECT * FROM alumnos_verificados where curso='$curso' and materia='$materia'"; /*and verificacion='0'*/
-if ($resultados = $con->query($sl)){
-    while ($fila = mysqli_fetch_array($resultados)){
-        $curso=$fila["curso"];
-        $materia=$fila["materia"];
-        $Email=$fila["Email"];
-        $_SESSION["u"]['materia']=$materia;
-
-        $ssql = "SELECT * FROM alumno where Email='$Email'";
-        if($resultadross = $con->query($ssql)){
-            if($resultadross ->num_rows > 0){
-               $fila=$resultadross->fetch_assoc(); 
-                $nombre=$fila["nombre"];
-                $apellido=$fila["apellido"];
+                $materia=$fila["materia"];
+                $curso=$fila["curso"];  
+                $sl =  "SELECT * FROM alumnos_verificados where curso='$curso' and materia='$materia'"; /*and verificacion='0'*/
+                
+                if ($resultados = $con->query($sl)){
+                    
+                    while ($fila = mysqli_fetch_array($resultados)){
+                        
+                        $curso=$fila["curso"];
+                        $materia=$fila["materia"];
+                        $Email=$fila["Email"];
+                        $_SESSION["u"]['materia']=$materia;
+                        $ssql = "SELECT * FROM alumno where Email='$Email'";
+                        
+                        if($resultadross = $con->query($ssql)){
+                            
+                            if($resultadross ->num_rows > 0){
+                                
+                                $fila=$resultadross->fetch_assoc(); 
+                                $nombre=$fila["nombre"];
+                                $apellido=$fila["apellido"];
+                            }
+                        }
+                        echo" 
+         
+                            <div class='contenedor-tabla row around-xs'> 
+                                <table class='col-xs-12'>
+                                    <tr >
+                                        <td class='col-xs-3'>  
+                                            ". $nombre ."<br> ". $apellido."
+                                        </td>
+                                        
+                                        <td class='col-xs-2'>
+                                            ".  $curso."
+                                        </td>
+                                        
+                                        <td class='col-xs-2'>  
+                                            ".  $materia."
+                                        </td> 
+                                        
+                                        <td class='col-xs-2'>
+                                            <a href='verificado_denegado.php?Email=$Email & materia=$materia' > 
+                                                <button>denegar</button>
+                                            </a> 
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>";
+                    } 
+                }
+            
             }
-        }
-        
-        echo"
-        
-        <div class='contenedor-tabla row around-xs'> 
-        <table class='col-xs-12'>
-           <tr >
-              <td class='col-xs-3'>  
-            ". $nombre ."<br> ". $apellido."
-              </td>  
-              <td class='col-xs-2'>
-            ".  $curso."
-              </td>
-              <td class='col-xs-2'>  
-            ".  $materia."
-              </td> 
-              <td class='col-xs-2'>
-              <a href='verificado_denegado.php?Email=$Email & materia=$materia' > <button>denegar</button></a> </td>
-          </tr>
-        </table>
-        </div>";
-}}
-}}   
+        }   
 ?>
 </div>
-</div>
+    </div>
 </body>
 </html>
