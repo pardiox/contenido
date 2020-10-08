d253acce39f0790eade54667dc8f1b87c913ed0f
LucasG32 <lucasg3322@gmail.com>
2020-10-08 02:27:17 -0300

    Update profesor_contenido_subido.php

--- profesor/profesor_contenido_subido.php
+++ profesor/profesor_contenido_subido.php
@@ -1,18 +1,21 @@
 <?php 
     session_start();
 	include "../conexion.php";
-
+    $fecha_de_subida = date('Y-m-d');
     $fecha_de_entrega=$_REQUEST['fecha_de_entrega'];
 	$titulo=$_REQUEST['titulo'];
 	$curso=$_REQUEST['curso'];
-	$materia=$_REQUEST['materia'];
+
+    if(isset($_REQUEST['materia'])){
+	   $materia=$_REQUEST['materia'];}
+    else{
+        header ("location:formulario_profesor.php");
+    }
 	$contenido=$_REQUEST['contenido'];
     $bibiografia=$_REQUEST['bibiografia'];
     $Email=$_SESSION["u"]['Email'];
-echo strlen($contenido);
-exit();
-	$SQL="INSERT INTO contenido (fecha_de_entrega, titulo, curso, materia, contenido, bibiografia, Email) VALUES
-	('$fecha_de_entrega', '$titulo', '$curso', '$materia', '$contenido','$bibiografia','$Email')";
+	$SQL="INSERT INTO contenido (fecha_de_entrega, fecha_de_subida, titulo, curso, materia, contenido, bibiografia, Email) VALUES
+	('$fecha_de_entrega', ('$fecha_de_subida'), '$titulo', '$curso', '$materia', '$contenido','$bibiografia','$Email')";
 	
 	if(!$error= $con-> query($SQL)){
 		 echo $con->error;
