ec7c51820f521a6b6f3e904159eff195b10ecfbf
LucasG32 <lucasg3322@gmail.com>
2020-10-08 02:27:00 -0300

    Update buscar_curso.php

--- profesor/buscar_curso.php
+++ profesor/buscar_curso.php
@@ -1,25 +1,28 @@
 <?php
     include "../conexion.php";
+    session_start();
     $query = "SELECT * FROM cursos_y_materia";    
     if (isset($_POST['consulta'])) {
     	$q = $_POST['consulta'];
-    	$query = "SELECT * FROM  cursos_y_materia WHERE curso LIKE '%$q%' or  materia LIKE '%$q%' ";
+        $profesor = $_SESSION["u"]['Email'];
+    	$query = "SELECT * FROM  cursos_y_materia WHERE (curso LIKE '%$q%' or  materia LIKE '%$q%') and (profesor='$profesor')";
         $resultado = $con->query($query);
     if ($resultado->num_rows>0) {
         
     	while ($fila = $resultado->fetch_assoc()) {
              $curso=$fila['curso'];
              $materia=$fila['materia'];
-    		?>                 
-                   
-                   <a href="clases.php?curso=<?php echo $curso;?> & materia=<?php echo $materia;?>"><p>
-                    <?php echo $fila['curso'], " ", $fila['materia'];?>
+            $respuesta = "
+            <a href='clases.php?curso=$curso & materia=$materia'><p>
+                    $curso $materia
                   </p></a>
-               
-            <?php
-       
+            ";
+       echo $respuesta;
     	}
+    }else{
+        echo "no hay resultados";
     }
+    
         $con->close();
     }
 
