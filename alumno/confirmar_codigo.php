54ed1f33efb4075729442fbeb0e0288eda2075a0
LucasG32 <lucasg3322@gmail.com>
2020-10-09 09:14:38 -0300

    Create confirmar_codigo.php

---
+++ alumno/confirmar_codigo.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+/*
+if(  $_SESSION["u"]['verificado'] == "1"){
+   header("Location: alumno.php");
+   exit();
+}
+*/
+?>
+
+
+<html lang="en">
+  <head>
+      <title>Verificar</title>  
+	  <!--comentario-->
+	  <meta http-equiv="Content-type" content="text/html;
+	  charset=utf-8">
+	  <link rel="stylesheet" type="text/css" href="css/estilologin1.css">
+      <link rel="stylesheet" type="text/css" href="css/estilo.css">
+  </head>
+  <body> 
+      <header>
+          <center>
+          <h1>E.E.S.T.N°14</h1>
+          <a href="cerrar_sesion.php">  <img src="img/logo.png" width="50px" height="auto"></a>
+          </center>
+      </header>
+    
+	<form action="verificar_alumno.php" method="post" class="form-register">
+     
+       <center>
+       <input type="text"  placeholder="&#128100; Inserte el codigo de verificacion" name="codigo">
+   	<input type="submit" value="enviar" class="boton">
+       	</center>
+       <br>
+    
+       	
+       </form> 
+       
+       
+<div class="footer">
+    facebook
+</div>
+     
+</body>
+</html>
