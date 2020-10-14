85ffdcf165e4e5bc1927d2d58c8c5ed0433509de
LucasG32 <lucasg3322@gmail.com>
2020-10-14 06:01:25 -0300

    Create cambiar_nombre_verificar.php

---
+++ alumno/cambiar_nombre_verificar.php
@@ -0,0 +1,31 @@
+<?php  
+        session_start();
+        include "../conexion.php";
+            $clave = $_REQUEST["clave"];
+            $nombre = $_REQUEST["nombre"];
+            $apellido = $_REQUEST["apellido"];
+            $Email= $_SESSION["u"]['Email'];
+            $sql =  "SELECT * from alumno where Email='$Email' and password='$clave'";
+            if ($resultado = $con->query($sql)){
+                if ($resultado->num_rows > 0){
+                        $SQL="UPDATE alumno SET nombre='$nombre' and apellido='$apellido' where Email='$Email'"; 
+                        if(!$error= $con-> query($SQL)){
+                            echo $con->error;
+	                    }
+                        else{
+                            $_SESSION["tipo"] = "nombre cambiado con exito";
+                            $_SESSION["u"]['nombre'] = $nombre;
+                            $_SESSION["u"]['apellido'] = $apellido;
+                            header("Location: cambiar_nombre.php"); 
+                            exit();
+	                   }
+                    }
+                    else{
+                        $_SESSION["tipo"] = "contraseña incorrecta";
+                    }
+                }
+               
+            
+       header("Location: cambiar_nombre.php");
+
+?>
