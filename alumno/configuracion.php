19d2e1b46460db07cfbb431346f7830efc40d19f
LucasG32 <lucasg3322@gmail.com>
2020-10-14 03:53:23 -0300

    Update configuracion.php

---
+++ alumno/configuracion.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+         <link rel="stylesheet" type="text/css" href="css/configuracion.css">
+    </head>
+    
+    <body>
+       
+        <header>
+            <h2>Carpeta virtual</h2>
+        </header>
+        
+        <center>
+            <div class="container">
+                <h2>Configuraciones</h2>
+                <a href="cambiar_contraseña.php"><h3>Cambiar contraseña</h3></a>
+                <a href="#"><h3>Editar nombre de usuario</h3></a>
+                <a href="#"><h3>reportar un error</h3></a>
+            </div>
+        </center>
+    </body>
+    
+</html>
