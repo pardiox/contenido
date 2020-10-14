e88faca4ce5682523ad54a9820db606532a07051
LucasG32 <lucasg3322@gmail.com>
2020-10-14 06:01:30 -0300

    Update configuracion.php

--- alumno/configuracion.php
+++ alumno/configuracion.php
@@ -6,6 +6,7 @@
     <body>
        
         <header>
+           <a href="alumno.php"><p>atras</p></a>
             <h2>Carpeta virtual</h2>
         </header>
         
@@ -13,8 +14,8 @@
             <div class="container">
                 <h2>Configuraciones</h2>
                 <a href="cambiar_contraseña.php"><h3>Cambiar contraseña</h3></a>
-                <a href="#"><h3>Editar nombre de usuario</h3></a>
-                <a href="#"><h3>reportar un error</h3></a>
+                <a href="cambiar_nombre.php"><h3>Editar nombre de usuario</h3></a>
+                <a href="reportes.php"><h3>reportar un error</h3></a>
             </div>
         </center>
     </body>
