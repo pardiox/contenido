4a27f5e7d8fc7b18992063ab3e944a575e31e445
LucasG32 <lucasg3322@gmail.com>
2020-10-15 05:49:32 -0300

    Update cambiar_contraseña_verificar.php

--- alumno/cambiar_contraseña_verificar.php
+++ alumno/cambiar_contraseña_verificar.php
@@ -4,15 +4,18 @@
         if(isset($_REQUEST["clave1"])){
             $clave1 = $_REQUEST["clave1"];
             $Email= $_SESSION["u"]['Email'];
-            $sql =  "SELECT * from alumno where Email='$Email' and password='$clave1'";
+            $sql =  "SELECT * from alumno where Email='$Email'";
             if ($resultado = $con->query($sql)){
                 if ($resultado->num_rows > 0){
+                    $fila=$resultado->fetch_assoc();
+                     $hash= $fila["password"];
+                    if (password_verify($clave1, $hash))  {      
                     if ($_REQUEST["clave2"] == $_REQUEST["clave3"]){
                     
                         $clave2 = $_REQUEST["clave2"];
-                        $clave3 = $_REQUEST["clave3"];
-
-                        $SQL="UPDATE alumno SET password='$clave3' where Email='$Email'"; 
+                      
+                        $clave_nueva= password_hash($clave2, PASSWORD_DEFAULT, ['cost'=> 10]);
+                        $SQL="UPDATE alumno SET password='$clave_nueva' where Email='$Email'"; 
                         if(!$error= $con-> query($SQL)){
                             echo $con->error;
 	                    }
@@ -25,10 +28,11 @@
                     else{
                         $_SESSION["tipo"] = "las dos contraseñas tienen que ser diferente";
                     }
-                }
-                else{
+                }else{
                     $_SESSION["tipo"] = "debe ingresar su contraseña actual";
                 }
+                }
+                
             }
         }
         header("Location: cambiar_contraseña.php");
