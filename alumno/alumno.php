3d433d7b529e579dd13745d1aeeffe288319881d
m-alvarado <54638885+m-alvarado@users.noreply.github.com>
2020-10-16 03:51:58 -0300

    cambios en "alumno" tanto el php como su estilo

--- alumno/alumno.php
+++ alumno/alumno.php
@@ -35,9 +35,7 @@ if( $_SESSION["u"]['sesion'] != "s.a"){
             
         </div>
 	</header>
-	
-<div class="contenedor-principal row center-xs around-md">
-<div id="mySidenav" class="sidenav">
+	<div id="mySidenav" class="sidenav">
     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
     <a href="#">Clases</a>
     <a href="#" name="tarea">Tareas pendientes</a>
@@ -47,12 +45,18 @@ if( $_SESSION["u"]['sesion'] != "s.a"){
     <a href="#">Contact</a>
     <a href="../cerrar_sesion.php">cerrar sesion</a>
 </div>
+<div class="contenedor-principal row center-xs around-md">
+    <div class="perfil col-xs-11 col-md-4">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ea, quae suscipit quis sit obcaecati quaerat distinctio omnis est nam et inventore, maxime debitis, beatae ipsam repellendus error esse soluta.</p>
+    </div>
 
 
 <div class="body col-xs-11 col-md-6">
     <div class="top-body row center-xs middle-xs ">
         <h2>Cursos</h2>
+       
     </div>
+    
     <div class="cursos row center-xs">
 <?php
      $sql =  "SELECT * FROM cursos_y_materia where curso='$curso'";
@@ -78,39 +82,36 @@ if( $_SESSION["u"]['sesion'] != "s.a"){
                     if ($resultado->num_rows > 0){
                         $fila=$resultado->fetch_assoc();
                         $fecha_de_subida=$fila["fecha_de_subida"];
-                        $fecha_de_entrega=$fila["fecha_de_entrega"];
+                       
+                       $fecha_de_entrega=$fila["fecha_de_entrega"];
                     }else{
-                        $fecha_de_subida="pronto habran nuevos trabajos";
-                        $fecha_de_entrega="";
+                        $fecha_de_subida="No hay ningun trabajos disponible";
+                        
+                        $fecha_de_entrega="No hay trabajos disponibles";
                     }
                 }
 ?>
 <div class="materia col-xs-11" >
-        <a href="ver_contenido.php? Email=<?php echo $Email;?> & materia=<?php echo $materia;?>">
-            
-                <h2><?php echo $materia; ?> </h2>
-                <p><?php echo $nombre, " ", $apellido; ?></p>
-                <p><?php echo $fecha_de_subida, " ", $fecha_de_entrega; ?></p>
-            
-
-        <a href="ver_contenido.php?Email=<?php echo $Email;?> & materia=<?php echo $materia;?>">
-          <!---
-          celular - xs
-          tablet - sm
-          un poquito mas grande - md
-          muy grande - lg
-          -->
-          
-           
-            <div class="materia" >
-                <h2><?php echo $materia; ?> </h2>
-                <p><?php echo $nombre, " ", $apellido; ?></p>
-                <p><?php echo $fecha_de_subida, " ", $fecha_de_entrega; ?></p>
+    <div class="overlay col-xs-12">
+        <div class="titulo-materia row center-xs">
+            <h2><?php echo $materia; ?> </h2>
+        </div>
+        <div class="materia-info row start-md center-xs">
+            <p><?php echo "Profesor: ", $nombre, " ", $apellido; ?></p>
+            <p></p>
+            <p><?php echo "Ultimo trabajo ", $fecha_de_entrega; ?></p>
+        </div>
+        <div class="ir-materia row center-xs middle-xs">
+           <div class="btn-ir col-xs-3">
+            <a href="ver_contenido.php? Email=<?php echo $Email;?> & materia=<?php echo $materia;?>">
+                Ir a la carpeta
+            </a>
             </div>
-
+        </div>
+        <div class="materia-bottom col-xs-12">
             
-        </a>
-        </a>
+        </div>
+    </div>
 </div>
 <?php }} ?>
 
@@ -127,10 +128,7 @@ function closeNav() {
 </script>
    </div>
 </div>
-<div class="perfil col-xs-12 col-md-4">
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ea, quae suscipit quis sit obcaecati quaerat distinctio omnis est nam et inventore, maxime debitis, beatae ipsam repellendus error esse soluta.</p>
- 
-</div>
+
 </div>
 <div class="footer ">
        <footer class="col-xs-12">
