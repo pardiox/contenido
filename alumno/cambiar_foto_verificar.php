26de00f4b2498de3d2d15db7ba678864771bab12
LucasG32 <lucasg3322@gmail.com>
2020-10-17 09:16:31 -0300

    Update cambiar_foto_verificar.php

--- alumno/cambiar_foto_verificar.php
+++ alumno/cambiar_foto_verificar.php
@@ -1,23 +1,38 @@
 <?php  
         session_start();
         include "../conexion.php";
-        if(isset($_REQUEST["foto"])){
-            $foto = $_REQUEST["foto"];
+       
+            echo $_FILES['imagen1']['name'];
+            echo $_FILES['imagen1']['tmp_name'];
+            echo $_FILES['imagen1']['type'];
+            echo $_FILES['imagen1']['size'];
+            echo $_FILES['imagen1']['error'];
             $Email= $_SESSION["u"]['Email'];
-     
-                        $biografia = $_REQUEST["biografia"];
-                        $SQL="UPDATE alumno SET foto='$foto' where Email='$Email'"; 
+            $extensiones = array(0=>'image/jpg',1=>'image/jpeg',2=>'image/png');
+            $max_tamanyo = 1024 * 1024 * 8;
+            $ruta_indexphp = dirname(realpath(__FILE__));
+            $ruta_fichero_origen = $_FILES['imagen1']['tmp_name'];
+            $ruta_nuevo_destino = $ruta_indexphp . '/imG/' . $_FILES['imagen1']['name'];
+            if ( in_array($_FILES['imagen1']['type'], $extensiones) ) {
+                echo 'Es una imagen';
+                if ( $_FILES['imagen1']['size']< $max_tamanyo ) {
+                    if( move_uploaded_file ( $ruta_fichero_origen, $ruta_nuevo_destino ) ) {
+                      $SQL="UPDATE alumno SET foto='$foto' where Email='$Email'"; 
                         if(!$error= $con-> query($SQL)){
                             echo $con->error;
 	                    }
                         else{
-                           
+                           $_SESSION["u"]['foto']=$_FILES['imagen1']['name'];
                             $_SESSION["tipo"] = "foto editada con exito";
                             header("Location: cambiar_foto.php"); 
                             exit();
+                    }
+                }
+            }
+                        
 	                   }
 
-                }
+                
                 
             
         
