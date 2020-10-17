be4ef5018fa150ee1c729d6046c610ef5e2ecfc5
LucasG32 <lucasg3322@gmail.com>
2020-10-17 11:08:29 -0300

    Update ver_contenido.php

--- alumno/ver_contenido.php
+++ alumno/ver_contenido.php
@@ -1,13 +1,93 @@
 <?php
 include "../conexion.php";
 session_start();
-$Email=$_REQUEST["Email"];
+$Email_profe=$_REQUEST["Email"];
 $materia=$_REQUEST["materia"];
 $curso=$_SESSION["u"]['curso'];
-echo $Email;
-if(empty($Email) || empty($materia)){
+if(empty($Email_profe) || empty($materia)){
     header("Location: alumno.php");
-}else{
-    echo "hola";
 }
-?>
+    ?>
+<html>
+   <head>
+         <meta http-equiv="Content-type" content="text/html;
+	  charset=utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="css/seleccion_trabajo.css">
+        <link rel="stylesheet" type="text/css" href="css/header.css"  >
+   </head>
+   <body>
+      <header>
+        <div class="header ">
+            <div id="main">
+               <button>
+                <span style="font-size:30px;cursor:pointer; color:white;" onclick="openNav()" >&#9776; open</span>
+                </button>
+                     <h2>Carpeta virtual</h2>
+               
+            </div>
+            
+        </div>
+	</header>
+      <div id="mySidenav" class="sidenav">
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+    <Updated upstream></Updated>
+    <a href="configuracion.php">configuracion</a>
+    <a href="#">Contact</a>
+    <a href="../cerrar_sesion.php">cerrar sesion</a>
+</div>
+       
+       <div class="cuerpo">
+       <center>
+       <table class="table">
+           <tr class="tabla_arriba">
+               <td>titulo</td>
+               <td>fecha de entrega</td>
+               <td>fecha de subida</td>
+           </tr>
+        <?php
+        $sql =  "SELECT * FROM contenido where curso='$curso' and Email='$Email_profe'";    
+        if ($resultad = $con->query($sql)){
+            while ($fila = mysqli_fetch_array($resultad)){
+                $titulo=$fila["titulo"];
+                $materia=$fila["materia"];
+                $fecha_de_entrega=$fila["fecha_de_entrega"];
+                $fecha_de_subida=$fila["fecha_de_subida"];
+                ?>
+                <tr class="tabla_abajo">
+                    <td>
+                        <?php echo $titulo;?>
+                    </td>
+                    <td>
+                        <?php echo $fecha_de_entrega;?>
+                    </td>
+                    <td>
+                        <?php echo $fecha_de_subida;?>
+                    </td>
+                    <td class="button">
+                        <button>ver</button>
+                    </td>
+                </tr>
+                
+                <?php
+            }
+        }
+        ?>
+        </table>
+        </center>
+        </div>
+        
+<script>
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+</script>
+    </body>
+</html>
