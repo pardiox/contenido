7d3addd1b322505ce0e1d512c77fb73c164818ea
pardiox <39572582+pardiox@users.noreply.github.com>
2020-10-24 18:08:39 -0300

    Revert "icono temporal"
    This reverts commit 267f674964c07e6cfe2331aebb17a8cf304ef055.

--- index.php
+++ index.php
@@ -17,7 +17,6 @@ if( $_SESSION["u"]['sesion'] == "s.p"){
 <!DOCTYPE html>  
 <html lang="en">
 <head>
-    <link rel="icon" href="img/icono.ico">
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
